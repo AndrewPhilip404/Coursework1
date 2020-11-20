a3dcd51f4caaca6067bb19fd7e0c9b7939752b9c
Aachal-d <71809210+Aachal-d@users.noreply.github.com>
2020-11-20 22:47:39 +0400

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,9 @@
+//initializing objects
+var burger    = document.getElementById('burger'),
+    player     = document.getElementById('player'),
+    background     = document.getElementById('background'),
+    gameover  = document.getElementById('gameover')
+var left = 10;
+player.style.left = 10;
+player.style.top = 480;
+burger.style.top = 10;
