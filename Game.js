e34b74b54be57829262e23975ba86e0a5b648c82
dam18-byte <71809108+dam18-byte@users.noreply.github.com>
2020-11-20 23:48:02 +0400

    Add files via upload

--- Game.js
+++ Game.js
@@ -3,7 +3,15 @@ var burger    = document.getElementById('burger'),
     player     = document.getElementById('player'),
     background     = document.getElementById('background'),
     gameover  = document.getElementById('gameover')
+
+
 var left = 10;
 player.style.left = 10;
 player.style.top = 480;
 burger.style.top = 10;
+burger.style.left = Math.floor((Math.random() * 390) + 5);
+burger.style.width = "70px";
+burger.style.height = "70px";
+gameover.style.top = 150;
+gameover.style.left = 150
+
